6e1b349b3ce052b986deee0aa7c40774549bdc01
Md. Nayim Islam <77149380+MdNayimIslam@users.noreply.github.com>
2023-02-16 11:29:17 +0600

    Add files via upload

---
+++ grocerystore/footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+   <section class="box-container">
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="wishlist.php"> <i class="fas fa-angle-right"></i> wishlist</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> register</a>
+      </div>
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="tel: +8801755900263" target="_blank"><i class="fas fa-phone"></i>01755900263</a>
+         <a href="tel: +8801617450177" target="_blank"><i class="fas fa-phone"></i>01617450177</a>
+         <a href="mailto: shirminakter2018@gmail.com" target="_blank"> <i class="fas fa-envelope"></i>shirminakter2018@gmail.com</a>
+         <a href="https://www.google.com/maps/place/Department+of+CSE,+Varendra+University/@24.362153,88.6208815,17z/data=!4m6!3m5!1s0x39fbefb6afccfcd5:0x39656bbb38ffc96a!8m2!3d24.3621482!4d88.6253662!16s%2Fg%2F11ghncb66l?coh=164777&entry=tt" target="_blank"> <i class="fas fa-map-marker-alt"></i>Rajshahi, bangladesh - 6000</a>
+      </div>
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/shirminakter.nitu.3" target="_blank"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://www.instagram.com/shirmin.nitu" target="_blank"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/shirmin.nitu" target="_blank"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.instagram.com/shirmin.nitu" target="_blank"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+   </section>
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?> by <span>Shirmin akter web designer</span> | all rights reserved! </p>
+</footer>
